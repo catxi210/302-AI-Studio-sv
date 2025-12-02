f96f325324b136c3c9f920c9e4e4e89f20263dbd
JI4JUN <ji4jun0097@gmail.com>
2025-12-02 18:05:20 +0800

    chore(version): bump version to 25.48.1-beta.2

--- src/lib/app-info.ts
+++ src/lib/app-info.ts
@@ -4,7 +4,7 @@
  */
 export const appInfo = {
 	productName: "302 AI Studio",
-	version: "25.48.1-beta.1",
+	version: "25.48.1-beta.2",
 	description: "302 AI Studio",
 	author: {
 		name: "302.AI",
